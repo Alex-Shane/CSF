04a73036be410a5e7e3614f06fe27b936510d18d
Alex Shane <ashane1230@gmail.com>
2023-09-21 18:05:01 -0400

    attempted to start writing cmain

--- csf_assign02/c_wcmain.c
+++ csf_assign02/c_wcmain.c
@@ -14,7 +14,35 @@ int main(int argc, char **argv) {
   const unsigned char *best_word = (const unsigned char *) "";
   uint32_t best_word_count = 0;
 
-  // TODO: implement
+  struct WordEntry *words[HASHTABLE_SIZE];
+  for (int x = 0; x < HASHTABLE_SIZE; x++) {
+    words[x] = NULL;
+  }
+
+  // open file and get first word
+
+  while (wc_readnext(file, next_word) != 0) {
+    total_words++;
+    wc_tolower(next_word);
+    wc_trim_non_alpha(next_word);
+    int inserted;
+    struct WordEntry* bucket = wc_dict_find_or_insert(words, HASHTABLE_SIZE, next_word);
+    struct WordEntry* entry = wc_find_or_insert(bucket, next_word, &inserted);
+    entry->count++;
+    if (inserted == 1) {
+      unique_words++;
+    }
+    if (entry->count > best_word_count) {
+      best_word = next_word;
+    }
+    else if (entry->count == best_word_count) {
+      int compare = wc_str_compare(next_word, best_word);
+      if (compare < 0) {
+        best_word = next_word;
+      }
+    }
+  }
+
 
   printf("Total words read: %u\n", (unsigned int) total_words);
   printf("Unique words read: %u\n", (unsigned int) unique_words);
